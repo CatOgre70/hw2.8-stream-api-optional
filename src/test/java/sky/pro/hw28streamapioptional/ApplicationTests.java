33574fe9dd345325a2b4057775da5d47ab250219
Vasily Demin <vasily.demin@gmail.com>
2022-06-13 23:45:18 +0300

    Initial commit

---
+++ src/test/java/sky/pro/hw28streamapioptional/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.hw28streamapioptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
